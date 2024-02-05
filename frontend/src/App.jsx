b294a05a3ce2b45fd2e692270b9199624394f73e
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2024-02-05 21:17:59 +0000

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+import plot from 'react-plotly.js'
+import Form from './Form'
+const App = () => {
+  return (
+    <Form/>
+    
+  )
+}
+
+export default App
---
+++ slope_deflection/base/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['POST'])
+def getRoutes(request):
+    routes=[
+        'GET/api',
+    ]
+    return Response(routes)
+
+@api_view(['POST'])
+def get 
